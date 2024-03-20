fcad5234739a937b9fbc3e0d10d4b6307f05c585
axel <axeldemadryn@hotmail.com>
2024-03-20 19:05:08 -0300

    Ejercicio 3

--- lab-01/palabras.c
+++ lab-01/palabras.c
@@ -1,9 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
-int main(int argc, char *argv[])
+void imprimirPalabrasArreglo(char arrayPalabras[], int iFin)
 {
-    // Agregar código aquí.
+    int espacio = 0;
+
+    if (arrayPalabras[0] == ' ' || arrayPalabras[0] == '\t')
+       espacio = 1;
+
+    int i;
+    for (i = 0; i < iFin; i++)
+    {
+        if (arrayPalabras[i] != ' ' && arrayPalabras[i] != '\t')
+        {
+            printf("%c", arrayPalabras[i]);
+            if (espacio == 1)
+                espacio = 0;
+        }
+        else if (espacio == 0)
+        {
+            printf("\n");
+            espacio = 1;
+        }
+    }
+
+    // ultimo salto de linea si no hay espacio al final del texto
+    if (arrayPalabras[iFin - 1] != ' ' && arrayPalabras[iFin - 1] != '\t')
+        printf("\n");
+}
+
+int main()
+{
+    int c = 0;
+    char palabras[1000];
+
+    while ((char)(c) != EOF)
+    {
+        int i = 0;
+        do
+        {
+            c = getc(stdin);
+            palabras[i] = (char)(c);
+            i = i + 1;
+        } while ((char)(c) != '\n' && (char)(c) != EOF);
+
+        if ((char)(c) != EOF)
+        {
+            // se borra el caracter ENTER
+            palabras[i - 1] = '\0';
+            i = i - 1;
+            imprimirPalabrasArreglo(palabras, i);
+        }
+    }
+
+    printf("\n");
 
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
