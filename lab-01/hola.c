db01a44e02990c66431d6da68f4b9271963513ac
axel <axeldemadryn@hotmail.com>
2024-03-20 19:03:34 -0300

    Ejercicio 1

--- lab-01/hola.c
+++ lab-01/hola.c
@@ -3,8 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-    // Agregar código aquí.
-    printf("hola \n");
+    if (argc > 1) {
+
+       int i;
+       for (i = 1; i < argc; i++)
+        printf("%s ", argv[i]);
+
+       printf("\n");
+    }
 
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
