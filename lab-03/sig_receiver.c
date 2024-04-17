242129744a1890e6e1f630188fc4d61b818bddac
axel <axeldemadryn@hotmail.com>
2024-04-17 18:14:38 -0300

    Ejercicio 1

--- lab-03/sig_receiver.c
+++ lab-03/sig_receiver.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
 
-int main(int argc, char *argv[])
-{
+void myHandler(int sig) {
+    printf("Señal recibida: %d %s\n", sig, strsignal(sig));
+}
+
+int main(void) {
+
+    struct sigaction sa;
+    sa.sa_handler = myHandler;
+
+    while(1){
+      int i;
+      for (i = 1; i <= 64; i++) {
+         sigaction(i, &sa, NULL);
+      }
+
+      printf("Número de proceso: %d\n", getpid());
+      pause();
+    }
+    
     exit(EXIT_SUCCESS);
 }
