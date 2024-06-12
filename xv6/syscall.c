83e86c1135dd558c842f093a5913e6ac7a274fcc
axel <axeldemadryn@hotmail.com>
2024-06-11 21:53:26 -0300

    Ejercicio 3

--- xv6/syscall.c
+++ xv6/syscall.c
@@ -107,6 +107,7 @@ extern int sys_trace(void);
 extern int sys_answer(void);
 extern int sys_getppid(void);
 extern int sys_pscnt(void);
+extern int sys_pgcnt(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -133,7 +134,8 @@ static int (*syscalls[])(void) = {
 [SYS_trace]   sys_trace,
 [SYS_answer]   sys_answer,
 [SYS_getppid]  sys_getppid,
-[SYS_pscnt]  sys_pscnt
+[SYS_pscnt]  sys_pscnt,
+[SYS_pgcnt]  sys_pgcnt
 };
 
 extern int trace;
@@ -161,9 +163,10 @@ static char* syscalls_names[] = {
 [SYS_mkdir]   "sys_mkdir",
 [SYS_close]   "sys_close",
 [SYS_trace]   "sys_trace",
-[SYS_answer]   "sys_answer",
-[SYS_getppid]  "sys_getppid",
-[SYS_pscnt]  "sys_pscnt"
+[SYS_answer]  "sys_answer",
+[SYS_getppid] "sys_getppid",
+[SYS_pscnt]   "sys_pscnt",
+[SYS_pgcnt]   "sys_pgcnt"
 };
 
 void
